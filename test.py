23bda313aa9b76c888096ebf5a2c7ad493e3556b
yeguacelestial <yeguacelestial@gmail.com>
2020-02-29 14:38:40 -0600

    Created function to get installed software from Store

--- test.py
+++ test.py
@@ -1,22 +1,58 @@
 import wmi
+import winreg
 
 # Connection to local machine
 c = wmi.WMI()
 
-# List all running processes
-for process in c.Win32_Process():
-    print(process.ProcessId, process.Name)
+# # List all running processes
+# for process in c.Win32_Process():
+#     print(process.ProcessId, process.Name)
+#
+# # Percentage of free space for each fixed disk in the system
+# for disk in c.Win32_LogicalDisk(DriveType=3):
+#     print(disk.Caption, "%0.2f%% free" % (100.0*float(disk.FreeSpace) / float(disk.Size)))
+#
+# # Show IP and MAC addresses for IP-enabled network interfaces
+# for interface in c.Win32_NetworkAdapterConfiguration (IPEnabled=1):
+#     print (interface.Description, interface.MACAddress)
+#     for ip_address in interface.IPAddress:
+#         print (ip_address)
+#
+# # Display what's running on startup
+# for s in c.Win32_StartupCommand():
+#     print(f"[{s.Location}] {s.Caption} <{s.Command}>")
 
-# Percentage of free space for each fixed disk in the system
-for disk in c.Win32_LogicalDisk(DriveType=3):
-    print(disk.Caption, "%0.2f%% free" % (100.0*float(disk.FreeSpace) / float(disk.Size)))
+# # List registry keys on system
+# r = wmi.Registry()
+# result, names = r.GetStringValue(
+#     hDefKey = winreg.HKEY_LOCAL_MACHINE,
+#     sSubKeyName="Software"
+# )
+# for key in names:
+#     print(key)
 
-# Show IP and MAC addresses for IP-enabled network interfaces
-for interface in c.Win32_NetworkAdapterConfiguration (IPEnabled=1):
-    print (interface.Description, interface.MACAddress)
-    for ip_address in interface.IPAddress:
-        print (ip_address)
+def get_all_installed_win10_store():
+    results = {}
 
-# Display what's running on startup
-for s in c.Win32_StartupCommand():
-    print(f"[{s.Location}] {s.Caption} <{s.Command}>")
+    try:
+        for info in c.Win32_InstalledStoreProgram():
+            if '-' in info.Name:
+                continue
+            app_name = str(info.Name).encode('utf8', 'ignore').decode()
+
+            if not info.Vendor:
+                vender = 'None'
+            if '-' in info.Vendor:
+                vender = 'None'
+            else:
+                vendor = str(info.Vendor).encode('utf8', 'ignore').decode()
+                vender = vendor.split(', ')[0].split('=')[-1].strip()
+            if app_name in results:
+                continue
+            if app_name not in results:
+                results[app_name] = []
+
+            results[app_name] = [vender, info.Version, 'None']
+    except:
+        pass
+    return results
