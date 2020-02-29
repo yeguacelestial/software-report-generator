3f5182e7fc40cdf7ed936898fcda5b1d0e4196d4
yeguacelestial <yeguacelestial@gmail.com>
2020-02-29 14:07:22 -0600

    Tested free space for each fixed disk in system

--- test.py
+++ test.py
@@ -6,3 +6,7 @@ c = wmi.WMI()
 # List all running processes
 for process in c.Win32_Process():
     print(process.ProcessId, process.Name)
+
+# Percentage of free space for each fixed disk in the system
+for disk in c.Win32_LogicalDisk(DriveType=3):
+    print(disk.Caption, "%0.2f%% free" % (100.0*float(disk.FreeSpace) / float(disk.Size)))
