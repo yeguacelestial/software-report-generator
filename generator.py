7bf137ff2f44a73740c400ad31f04b26414fdb1b
yeguacelestial <yeguacelestial@gmail.com>
2020-04-01 00:29:28 -0500

    Created table generator

--- generator.py
+++ generator.py
@@ -84,7 +84,7 @@ class ReportGenerator():
         try:
             if direccion[-1] != '\\':
                 direccion = direccion + "\\"
-                
+
             if nombre == None:
                 nombre = "reporte"
 
@@ -95,10 +95,12 @@ class ReportGenerator():
             pdf = canvas.Canvas(direccion_final)
             print(f"[+] Generando reporte como {nombre}.pdf...")
 
+            table_data = [["APP", "FABRICANTE", "VERSION", "FECHA DE INSTALACION"]]
             def escribir(diccionario):
                 i = 0
 
                 for k, v in diccionario:
+                    table_data.append([k,v[0],v[1],v[2]])
                     pdf.setFont('Helvetica', 8)
                     pdf.drawString(50, 800-i, str(f'Aplicación: {k} - Fabricante: {v[0]} - Version: {v[1]} - Fecha de instalación: {v[2]}'))
                     i += 20
@@ -117,12 +119,57 @@ class ReportGenerator():
             escribir(apps_productos)
             escribir(apps_win10)
 
+        # START TABLE
+            from reportlab.platypus import SimpleDocTemplate
+            from reportlab.lib.pagesizes import letter
+
+            pdf_table = SimpleDocTemplate(
+                "table.pdf",
+                pagesize = letter
+            )
+
+            from reportlab.platypus import Table
+
+            from reportlab.lib.units import inch
+            colwidths = [2*inch] * len(table_data)
+            #rowheights = [.2*inch] * len(data)
+
+            table = Table(table_data, colwidths)
+
+            # Add style
+            from reportlab.platypus import TableStyle
+            from reportlab.lib import colors
+            style = TableStyle([
+                    ('BACKGROUND', (0,0), (3,0), colors.green),
+                    ('TEXTCOLOR', (0,0), (-1,0), colors.whitesmoke),
+
+                    ('ALIGN', (0,0), (-1,-1), 'CENTER'),
+
+                    ('FONTAME', (0,0), (-1,0), 'Courier-Bold'),
+                    ('FONTSIZE', (0,0), (-1,-1), 8),
+                    ('FONTSIZE', (0,1), (0,-1), 5),
+
+                    ('BOTTOMPADDING', (0,0), (-1,0), 12),
+
+                    ('BACKGROUND', (0,1), (-1,-1), colors.beige),
+            ])
+            table.setStyle(style)
+
+            elems = []
+            elems.append(table)
+
+            pdf_table.build(elems)
+
+            print("[+] Tabla generada.")
+        # END TABLE
             pdf.save()
 
             print(f"[+] Reporte {nombre}.pdf generado en {direccion_final}")
 
         except IOError as e:
-            print("[-] El directorio no existe. TIP: Si existe, escribe el directorio entre comillas.")
+            print(e)
+            print("[-] El directorio no existe, o no hay suficientes permisos para generar el reporte.")
+            print("[*] TIP: Si el directorio existe, escribelo entre comillas.")
 
         except Exception as e:
             print(e)
@@ -130,5 +177,6 @@ class ReportGenerator():
             exit
 
 # TODO:
+# - Tabular todos los datos
+#   * Corregir texto de la columna APP
 # - Espacio para la firma
-# - Inputs del usuario (direccion, nombre del archivo)
